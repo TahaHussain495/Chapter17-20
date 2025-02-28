9ac92a5d84a9c536e456be7d243c9d1c1a408d57
Taha Hussain <thussain170305@gmail.com>
2025-02-28 16:15:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,537 @@
+
+
+
+// question 3 
+// Write a program to print numeric counting from 1 to 10.
+
+
+// for ( let i = 1; i <= 10; i++) {
+//     console.log(i);
+    
+// }
+
+
+// question 4
+
+// Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+
+// let a = +prompt("Enter a number for the multiplication table")
+
+// for( let i = 1; i <= 10; i++) {
+//     console.log( a + " x " + i + " = " + (a * i) )
+    
+// }
+
+
+
+// question 5
+// Write a program to print items of the following array
+// using for loop:
+
+
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+// for( let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i] )
+// }
+
+// document.write("<h3> Element at index "+ [0] +" is apple </h3>")
+// document.write("<h3> Element at index "+ [1] +" is banana </h3>")
+// document.write("<h3> Element at index "+ [2] +" is mango </h3>")
+// document.write("<h3> Element at index "+ [3] +" is orange </h3>")
+// document.write("<h3> Element at index "+ [4] +" is stawberry </h3>")
+
+
+
+
+
+// question 6 
+
+// Generate the following series in your browser. See
+// example output.
+
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+
+// for ( let i = 1; i <= 15; i++) {
+//     console.log(i);
+    
+// }
+
+
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+
+
+// for( let i = 1; i < 11; i++) {
+//     console.log( 11 - i);
+    
+// }
+
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+
+// let num = +prompt("Enter a even or odd number")
+
+// if( num % 2 === 0) {
+//     console.log("This is even number") ;
+    
+// } else if ( num % 3 === 0) {
+//         console.log("This is odd number")
+//      }  
+       
+
+
+// e Serius
+
+// let num = [ 2 , 4 , 6 , 8 , 10 , 12 , 14 , 16]
+
+// let result = num.join(" k , ")
+
+// console.log(result);
+
+
+
+// 7 Write a program to enable “search by user input” in an array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example:
+
+
+// let arr = ["cake", "apple pie", "cookie", "chips", "patties"]
+// let a = prompt("Enter item")
+
+// let flag = false
+
+// for( let i = 0; i < arr.length; i++) {
+//     if ( arr[i] === a) {
+//         console.log( a + " is available");
+//         flag = "true"
+        
+//     }
+// }
+
+// if ( flag === false) {
+//     console.log(" Sorry! Its not available");
+    
+// }
+
+
+// 8
+
+// Write a program to identify the largest number in the
+// given array.
+
+
+// let num = [ 12 , 45 , 67 , 69 , 97 , 377 , 82 , 50]
+
+// let largest = num[0]
+
+// for( let i = 1; i < num.length; i++) {
+//     if (num[i] > largest){
+//         largest = num[i]
+//     }
+        
+// }
+
+// console.log("Largest nuumber is " , largest);
+
+
+
+// 9
+// Write a program to identify the smallest number in the
+// given array.
+
+
+// let a = [24, 53, 78, 91, 12]
+// let smallest = a[0]
+
+// for( let i = 0; i < a.length; i++) {
+//     if (a[i] < smallest) {
+//         smallest = a[i]
+//     }
+// }
+
+// console.log("Smallest number in the array is " , smallest);
+
+
+
+
+
+// 10
+
+// Write a program to print multiples of 5 ranging 1 to
+// 100.
+
+
+// let num = 5
+
+// for(let i = 1; i <= 20; i++) {
+//     console.log(num * i);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
